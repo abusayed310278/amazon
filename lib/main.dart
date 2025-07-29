14b8cb09c0bdd23b9bb00dd9c9ddfcbca5636666
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-07-29 14:40:17 +0600

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon/utils/theme.dart';
 import 'package:amazon/view/auth_screens.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,11 @@ class Amazon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme:theme,
         home:AuthScreen(),
         debugShowCheckedModeBanner: false,
     );
   }
 }
+
+
--- lib/view/auth_screens.dart
+++ lib/view/auth_screens.dart
@@ -10,8 +10,42 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
 
+    final height=MediaQuery.of(context).size.height;
+    final width=MediaQuery.of(context).size.width;
+    final textTheme=Theme.of(context).textTheme;
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title:Image(
+            image:AssetImage('assets/images/amazon_logo.png'),
+            height: height*0.04,
+            width: width,
+          ),
+        ),
+        body:SafeArea(
+          child: Container(
+            height: height,
+            width: width,
+            padding: EdgeInsets.symmetric(
+              horizontal: width*0.03,
+              vertical: height*0.02,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome',
+                  style:textTheme.displaySmall!.copyWith(fontWeight: FontWeight.w600),
+                ),
+
+              ],
+            ),
+          ),
+
+        ),
     );
   }
 }
