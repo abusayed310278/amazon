858eaa0f017ad5028bd27c1a744b5babb0cd853b
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-07-30 11:00:35 +0600

    login button design

--- lib/view/auth_screens.dart
+++ lib/view/auth_screens.dart
@@ -1,5 +1,6 @@
 import 'package:amazon/constants/common_functions.dart';
 import 'package:amazon/utils/colors.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -10,180 +11,264 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-
-  bool inLogin=false;
-
+  bool inLogin = false;
+  String currentCountryCode = '+88';
+  TextEditingController mobileController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
-    final height=MediaQuery.of(context).size.height;
-    final width=MediaQuery.of(context).size.width;
-    final textTheme=Theme.of(context).textTheme;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-        backgroundColor: white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          title:Image(
-            image:AssetImage('assets/images/amazon_logo.png'),
-            height: height*0.04,
-            width: width,
+      backgroundColor: white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Image(
+          image: AssetImage('assets/images/amazon_logo.png'),
+          height: height * 0.04,
+          width: width,
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.03,
+            vertical: height * 0.02,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome',
+                style: textTheme.displaySmall!.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              CommonFunctions.blankSpace(height * 0.02, 0),
+              Container(
+                width: width,
+                decoration: BoxDecoration(
+                  border: Border.all(color: greyShade3),
+                ),
+                child: Column(
+                  children: [
+                    // Create Account Option
+                    Container(
+                      height: height * 0.06,
+                      width: width,
+                      decoration: BoxDecoration(
+                        border: Border(bottom: BorderSide(color: greyShade3)),
+                        color: greyShade1,
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.03),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                inLogin = false;
+                              });
+                            },
+                            child: Container(
+                              height: height * 0.03,
+                              width: height * 0.03,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(color: grey),
+                                color: white,
+                              ),
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.circle,
+                                size: height * 0.015,
+                                color: inLogin ? transparent : secondaryColor,
+                              ),
+                            ),
+                          ),
+                          CommonFunctions.blankSpace(0, width * 0.02),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'Create Account. ',
+                                  style: textTheme.bodyMedium!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'New to Amazon?',
+                                  style: textTheme.bodyMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Sign In Option and Input Fields
+                    Container(
+                      width: width,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.03,
+                        vertical: height * 0.01,
+                      ),
+                      child: Column(
+                        children: [
+                          // Sign In Option
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    inLogin = true;
+                                  });
+                                },
+                                child: Container(
+                                  height: height * 0.03,
+                                  width: height * 0.03,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(color: grey),
+                                    color: white,
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.circle,
+                                    size: height * 0.015,
+                                    color: inLogin
+                                        ? secondaryColor
+                                        : transparent,
+                                  ),
+                                ),
+                              ),
+                              CommonFunctions.blankSpace(0, width * 0.02),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Sign in. ',
+                                      style: textTheme.bodyMedium!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: 'Already a Customer?',
+                                      style: textTheme.bodyMedium,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          CommonFunctions.blankSpace(height * 0.01, 0),
+
+                          // Phone Input Row
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  showCountryPicker(
+                                    context: context,
+                                    onSelect: (val) {
+                                      setState(() {
+                                        currentCountryCode =
+                                            '+${val.phoneCode}';
+                                      });
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                  height: height * 0.05,
+                                  width: width * 0.2,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: grey),
+                                    color: greyShade2,
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Text(
+                                    currentCountryCode,
+                                    style: textTheme.bodyMedium!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              CommonFunctions.blankSpace(0, width * 0.05),
+                              Expanded(
+                                child: SizedBox(
+                                  height: height * 0.06,
+                                  width: width * 0.2,
+                                  child: TextFormField(
+                                    controller: mobileController,
+                                    cursorColor: Colors.black,
+                                    style: textTheme.bodySmall,
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                      hintText: "Enter mobile number",
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: BorderSide(color: grey),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: BorderSide(
+                                          color: secondaryColor,
+                                        ),
+                                      ),
+                                      disabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: BorderSide(color: grey),
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                        borderSide: BorderSide(color: grey),
+                                      ),
+                                      contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 15,
+                                        vertical: 0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          CommonFunctions.blankSpace(height * 0.02, 0),
+
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(width * 0.88, height * 0.06),
+                              backgroundColor: Colors.amber,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero, // 👈 No rounding, square corners
+                              ),
+                            ),
+                            child: Text(
+                              'Continue',
+                              style: textTheme.displaySmall,
+                            ),
+                          ),
+                          
+                          // ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        body:SafeArea(
-          child: Container(
-            height: height,
-            width: width,
-            padding: EdgeInsets.symmetric(
-              horizontal: width*0.03,
-              vertical: height*0.02,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome',
-                  style:textTheme.displaySmall!.copyWith(fontWeight: FontWeight.w600),
-                ),
-                CommonFunctions.blankSpace(height*0.02, 0),
-                Container(
-                  width: width,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: greyShade3),
-                  ),
-                  child: Column(
-                     children: [
-                       Container(
-                         height: height*0.06,
-                         width: width,
-                         // color: greyShade1,
-                         decoration: BoxDecoration(
-                           border: Border(
-                             bottom: BorderSide(
-                               color:greyShade3,
-                             ),
-                           ),
-                           color: greyShade1,
-                         ),
-                         padding: EdgeInsets.symmetric(
-                           horizontal:width*0.03 ,
-                         ),
-                         child: Row(
-                           children: [
-                             InkWell(
-                               onTap:(){
-                                 setState(() {
-                                   inLogin=false;
-                                 });
-                               },
-                               child: Container(
-                                 height: height*0.03,
-                                 width: height*0.03,
-                                 decoration: BoxDecoration(
-                                   shape: BoxShape.circle,
-                                   border: Border.all(
-                                     color: grey,
-                                   ),
-                                   color:white),
-                                   alignment: Alignment.center,
-                                   child:Icon(
-                                       Icons.circle,
-                                       size:height*0.015,
-                                        color:inLogin?transparent : secondaryColor,
-                                   ),
-                               ),
-                             ),
-                             CommonFunctions.blankSpace(0, width*0.02),
-                             RichText(
-                               text: TextSpan(
-                                 children: [
-                                   TextSpan(
-                                       text:'Create Account.',
-                                        style:textTheme.bodyMedium!
-                                          .copyWith(fontWeight: FontWeight.bold),
-
-                                   ),
-                                   TextSpan(
-                                     text:'New to Amazon?',
-                                     style:textTheme.bodyMedium,
-
-                                   ),
-                                 ],
-                               ),
-                             ),
-                           ],
-                         ),
-                       ),
-                       Container(
-                         width: width,
-                         padding: EdgeInsets.symmetric(
-                           horizontal: width*0.03,
-                           vertical: height*0.01,
-                         ),
-                         child: Column(
-                           children: [
-                             Row(
-                               children: [
-                                 InkWell(
-                                   onTap:(){
-                                     setState(() {
-                                       inLogin=true;
-                                     });
-                                   },
-                                   child: Container(
-                                     height: height*0.03,
-                                     width: height*0.03,
-                                     decoration: BoxDecoration(
-                                         shape: BoxShape.circle,
-                                         border: Border.all(
-                                           color: grey,
-                                         ),
-                                         color:white),
-                                     alignment: Alignment.center,
-                                     child:Icon(
-                                       Icons.circle,
-                                       size:height*0.015,
-                                       color:inLogin?secondaryColor:transparent,
-                                     ),
-                                   ),
-                                 ),
-                                 CommonFunctions.blankSpace(0, width*0.02),
-                                 RichText(
-                                   text: TextSpan(
-                                     children: [
-                                       TextSpan(
-                                         text:'Sign in.',
-                                         style:textTheme.bodyMedium!
-                                             .copyWith(fontWeight: FontWeight.bold),
-
-                                       ),
-                                       TextSpan(
-                                         text:'Already a Customer?',
-                                         style:textTheme.bodyMedium,
-
-                                       ),
-                                     ],
-                                   ),
-                                 ),
-                               ],
-                             ),
-                           ],
-                         ),
-                       ),
-                     ],
-
-
-
-                  ),
-                ),
-
-              ],
-            ),
-          ),
-
-        ),
+      ),
     );
   }
 }
