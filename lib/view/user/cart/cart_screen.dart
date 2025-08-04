0e4cb4a1d6a8ccd568f505e4deca2e6265f951bf
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-08-04 18:24:49 +0600

    cart item selected in cart page

--- lib/view/user/cart/cart_screen.dart
+++ lib/view/user/cart/cart_screen.dart
@@ -34,91 +34,138 @@ class _CartScreenState extends State<CartScreen> {
           height: height,
           width: width,
 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              RichText(
-                  text: TextSpan(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            
+                RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Subtotal ',
+                          style: textTheme.bodyLarge,
+                        ),
+                        TextSpan(
+                          text: '\$1800',
+                          style: textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
+                CommonFunctions.blankSpace(height*0.01, 0),
+            
+                SizedBox(
+                  height: height*0.06,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      TextSpan(
-                        text: 'Subtotal ',
-                        style: textTheme.bodyLarge,
-                      ),
-                      TextSpan(
-                        text: '\$1800',
-                        style: textTheme.bodyLarge!.copyWith(
-                          fontWeight: FontWeight.bold,
+                      Icon(Icons.check_circle,color:teal),
+                      CommonFunctions.blankSpace(0, width*0.02),
+            
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RichText(
+                              textAlign: TextAlign.justify,
+                              text: TextSpan(
+                                children: [
+            
+                                  TextSpan(
+                                    text:'Your Order is eligible for FREE Delivery.',
+                                    style: textTheme.bodySmall!.copyWith(
+                                      color: teal,
+            
+                                    )
+                                  ),
+                                  TextSpan(
+                                      text:'Select this options at checkout.',
+                                      style: textTheme.bodySmall!.copyWith(
+                                        color: black,
+            
+                                      )
+                                  ),
+            
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
+            
+            
                     ],
                   ),
-              ),
-              CommonFunctions.blankSpace(height*0.01, 0),
+                ),
+            
+                CommonFunctions.blankSpace(height*0.01, 0),
+            
+                ElevatedButton(
+                  onPressed: () {  },
+            
+                  style: ElevatedButton.styleFrom(
+                    shape:RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: amber,
+                    minimumSize: Size(
+                      width,height*0.06
+                    ),
+                  ),
+                  child: Text(
+                    'Proceed to Buy(2 items)',
+                    style: textTheme.bodyMedium,
+                  ),
+            
+                ),
+            
+                CommonFunctions.blankSpace(height*0.02, 0),
+            
+                CommonFunctions.divider(),
+            
+                CommonFunctions.blankSpace(height*0.02, 0),
+            
+                ListView.builder(
+                  itemCount: 2,
+                  shrinkWrap: true,
+                  itemBuilder: (context,index){
+                    return Container(
+                      height: height*0.2,
+                      width: width,
+                      margin: EdgeInsets.symmetric(
+                        vertical: height*0.01,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: greyShade1
+                      ),
 
-              SizedBox(
-                height: height*0.06,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(Icons.check_circle,color:teal),
-                    CommonFunctions.blankSpace(0, width*0.02),
-
-                    Expanded(
-                      child: Column(
+                      child: Row(
                         children: [
-                          RichText(
-                            textAlign: TextAlign.justify,
-                            text: TextSpan(
+                          Expanded(
+                            flex: 4,
+                            child: Column(
                               children: [
-
-                                TextSpan(
-                                  text:'Your Order is eligible for FREE Delivery.',
-                                  style: textTheme.bodySmall!.copyWith(
-                                    color: teal,
-
-                                  )
+                                Image(
+                                  image:AssetImage(
+                                    'assets/images/todays_deals/todaysDeal0.png'
+                                  ),
+                                  fit: BoxFit.fitWidth,
                                 ),
-                                TextSpan(
-                                    text:'Select this options at checkout.',
-                                    style: textTheme.bodySmall!.copyWith(
-                                      color: black,
-
-                                    )
-                                ),
-
                               ],
                             ),
                           ),
                         ],
                       ),
-                    ),
+                    );
 
-
-                  ],
+                  },
                 ),
-              ),
-
-              CommonFunctions.blankSpace(height*0.01, 0),
-
-              ElevatedButton(
-                onPressed: () {  },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: amber,
-                  minimumSize: Size(
-                    width,height*0.06
-                  ),
-                ),
-                child: Text(
-                  'Proceed to Buy(2 items)',
-                  style: textTheme.bodyMedium,
-                ),
-
-              ),
-
-              CommonFunctions.blankSpace(height*0.01, 0),
-
-            ],
+            
+              ],
+            ),
           ),
         ),
 
