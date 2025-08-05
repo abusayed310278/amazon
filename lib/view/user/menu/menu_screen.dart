0e690df7426ba607cf3916febc02c2f317ab08a1
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-08-05 22:07:57 +0600

    add

--- lib/view/user/menu/menu_screen.dart
+++ lib/view/user/menu/menu_screen.dart
@@ -98,7 +98,7 @@ class _MenuScreenState extends State<MenuScreen> {
                           ),
                           const Spacer(),
                           Icon(Icons.chevron_right,color: black,),
-                          
+
                         ],
                       ),
                     );
