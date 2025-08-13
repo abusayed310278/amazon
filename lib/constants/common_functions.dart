a2830839af1af68c42c6a2ff47daa1c03f77f2a2
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-08-13 13:12:21 +0600

    add

--- lib/constants/common_functions.dart
+++ lib/constants/common_functions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:motion_toast/motion_toast.dart';
 
 import '../utils/colors.dart';
 
@@ -12,20 +13,48 @@ class CommonFunctions {
     return Divider(thickness: 2, color: greyShade3, height: 0);
   }
 
-  static showToast({required BuildContext context,required String message}){
+  static showToast({required BuildContext context, required String message}) {
     return Fluttertoast.showToast(
-        msg: message,
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.TOP,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.white,
-        textColor: black,
-        fontSize: 16.0
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.white,
+      textColor: black,
+      fontSize: 16.0,
     );
   }
 
+  static showSuccessToast({
+    required BuildContext context,
+    required String message,
+  }) {
+    return MotionToast.success(
+      title: const Text('Success'),
+      description: Text(message),
+      // position: MotionToastPosition.top,
+    ).show(context);
+  }
 
+  static showErrorToast({
+    required BuildContext context,
+    required String message,
+  }) {
+    return MotionToast.error(
+      title: const Text('Error'),
+      description: Text(message),
+      // position: MotionToastPosition.top,
+    ).show(context);
+  }
 
-
-
+  static showWarningToast({
+    required BuildContext context,
+    required String message,
+  }) {
+    return MotionToast.warning(
+      title: const Text('Opps!'),
+      description: Text(message),
+      // position: MotionToastPosition.top,
+    ).show(context);
+  }
 }
